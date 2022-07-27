e5bfab0a6f00fd3e8bdd7862c46a3b02461016a1
Ahmet Bilgin <74270412+ahmetcbilgin@users.noreply.github.com>
2022-07-27 15:18:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function yazdir(){
+
+    var a = document.getElementById("adsoyad");
+    var b = document.getElementById("email");
+    var c = document.getElementById("konu");
+    var d = document.getElementById("mesaj");
+
+    alert("Ad Soyad         : " + a.value + "\n"+ "E-Posta adresi : " + b.value + "\n" +"Konu                : " + c.value + "\n" + "Mesaj               : " + d.value);
+    
+}
